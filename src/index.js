e6fe3ba772cfb5b3e2bde4ba6d862bf1fed1c240
Bruna <brustalli@icloud.com>
2020-08-14 11:16:58 +0100

    Changing the forecast. Adding real data.

--- src/index.js
+++ src/index.js
@@ -18,12 +18,52 @@ let weekDay = document.querySelector("#actual-day");
 let currentTime = new Date();
 weekDay.innerHTML = formatDate(currentTime);
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours;
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
+  let minutes = date.getMinutes;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = `9c9113d065806620d583cdfa492f9ff6`;
   let units = `metric`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
+
+function displayForecast(response) {
+  let forecast = response.data.list[0];
+  console.log(forecast);
+  document.querySelector(`#forecast`).innerHTML = `<div class="col-5">
+    <p class="hours">
+      ${formatHours(forecast.dt * 1000)}
+          </p>
+  </div>
+    <div class="col-2">
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+        class="iconSixDays"
+        id="icon"
+      />
+    </div>
+    <div class="col-5">
+      <p class="tempDays"><strong>${Math.round(
+        forecast.main.temp_max
+      )}° </strong> ${Math.round(forecast.main.temp_min)}°</p>
+    </div>`;
+}
+
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-text-input").value;
